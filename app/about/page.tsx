29a5236572a7b74b1c1fa64f8a0d986857e1212d
kingjamin-sketch <asingizwejamin@gmail.com>
2026-07-25 14:38:07 +0200

    Serve current About content dynamically

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,8 @@
 import AboutPageClient from "./AboutPageClient";
 import { getAboutPageContent } from "@/sanity/lib/siteContent";
 
+export const dynamic = "force-dynamic";
+
 export default async function AboutPage() {
   const content = await getAboutPageContent();
   return <AboutPageClient content={content} />;
