3c3edfa48376443bb5eaab8eec8975b578cd8f18
kingjamin-sketch <asingizwejamin@gmail.com>
2026-07-25 14:37:42 +0200

    Serve homepage content dynamically

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HomePageClient from "./HomePageClient";
 import { getHomePageContent } from "@/sanity/lib/siteContent";
 import { getFeaturedProjects } from "@/sanity/lib/projects";
 
-export const revalidate = 60;
+export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
   const [content, featuredProjects] = await Promise.all([
