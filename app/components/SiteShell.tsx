3798f5b9dde7b97afd4597c442b0a5c601a5e21a
kingjamin-sketch <asingizwejamin@gmail.com>
2026-08-23 22:36:03 +0200

    Route-split noncritical enhancement modules

--- app/components/SiteShell.tsx
+++ app/components/SiteShell.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type { ReactNode } from "react";
+import dynamic from "next/dynamic";
 import { MotionConfig } from "framer-motion";
 import { usePathname } from "next/navigation";
 
@@ -9,13 +10,20 @@ import DomicileWidget from "./DomicileWidget";
 import IMVOFinalRefinements from "./IMVOFinalRefinements";
 import IMVOPreviewCorrections from "./IMVOPreviewCorrections";
 import IMVOPreviewExperience from "./IMVOPreviewExperience";
-import IMVOStudioMetrics from "./IMVOStudioMetrics";
-import IMVOStudioPhotography from "./IMVOStudioPhotography";
 import IntroLoader from "./IntroLoader";
 import SiteFooter from "./SiteFooter";
 import SiteHeader from "./SiteHeader";
 import SmoothScrollProvider from "./SmoothScrollProvider";
 import type { SiteSettings } from "@/sanity/types/siteContent";
+
+const IMVOStudioPhotography = dynamic(() => import("./IMVOStudioPhotography"), {
+  ssr: false,
+});
+const IMVOStudioMetrics = dynamic(() => import("./IMVOStudioMetrics"), {
+  ssr: false,
+});
+
+const photographyRoutes = new Set(["/", "/about", "/services", "/contact"]);
 
 export default function SiteShell({
   children,
@@ -43,9 +51,9 @@ export default function SiteShell({
         <IMVOPreviewCorrections />
         <main id="main-content" tabIndex={-1}>
           {children}
-          <IMVOStudioPhotography />
+          {photographyRoutes.has(pathname) ? <IMVOStudioPhotography /> : null}
           <IMVOFinalRefinements />
-          <IMVOStudioMetrics />
+          {pathname === "/" ? <IMVOStudioMetrics /> : null}
         </main>
         <SiteFooter settings={settings} />
         <BackToTop />
