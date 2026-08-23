fba3dd803bae6e0be116f73d90ba56861a45fe66
kingjamin-sketch <asingizwejamin@gmail.com>
2026-08-24 01:21:48 +0200

    Enable ISR for projects index

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,7 @@
 import ProjectsPageClient from "./ProjectsPageClient";
 import { getAllProjects } from "@/sanity/lib/projects";
 
-export const dynamic = "force-dynamic";
+export const revalidate = 300;
 
 export default async function ProjectsPage() {
   const projects = await getAllProjects();
