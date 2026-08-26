ad4c17a2e9ca37de49c54d4d1e2dd6e69d4b56dc
kingjamin-sketch <asingizwejamin@gmail.com>
2026-08-26 11:03:49 +0200

    Wire structured team and homepage controls

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,29 @@
 import HomePageClient from "./HomePageClient";
+import HomeSectionVisibility from "./components/HomeSectionVisibility";
 import styles from "./home-lcp.module.css";
 import { getHomePageContent } from "@/sanity/lib/siteContent";
 import { getFeaturedProjects } from "@/sanity/lib/projects";
+import { getHomePageControls, getTeamMembers } from "@/sanity/lib/cmsBackend";
+import type { HomePageContent } from "@/sanity/types/siteContent";
 
 export const revalidate = 300;
 
 export default async function HomePage() {
-  const [content, featuredProjects] = await Promise.all([
+  const [content, featuredProjects, structuredTeam, controls] = await Promise.all([
     getHomePageContent(),
     getFeaturedProjects(3),
+    getTeamMembers(),
+    getHomePageControls(),
   ]);
+
+  const resolvedContent: HomePageContent | null = structuredTeam.length
+    ? { ...(content || {}), teamMembers: structuredTeam }
+    : content;
 
   return (
     <div className={styles.homePageFirstPaint}>
-      <HomePageClient content={content} featuredProjects={featuredProjects} />
+      <HomePageClient content={resolvedContent} featuredProjects={featuredProjects} />
+      <HomeSectionVisibility controls={controls} content={resolvedContent} />
     </div>
   );
 }
