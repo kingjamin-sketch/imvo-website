cc868fabdd77c0721258ed3608674d2bd1d2ec26
kingjamin-sketch <asingizwejamin@gmail.com>
2026-09-19 11:33:45 +0200

    Add IMVO Systems route layout

---
+++ app/systems/layout.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+import "../applied/applied.css";
+import "../applied/applied-refresh.css";
+
+export default function SystemsLayout({ children }: { children: ReactNode }) {
+  return children;
+}
